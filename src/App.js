d26211b18db6f496a4ea8e3624ca81507e57805f
emmelleppi <mlp.perego@gmail.com>
2020-03-01 02:53:46 +0100

    Adds background, text and effects

--- src/App.js
+++ src/App.js
@@ -1,127 +1,37 @@
-import React, { useEffect, useState, useRef, useMemo } from "react";
-import { Canvas, useFrame } from "react-three-fiber";
+import React, { Suspense } from "react";
+import { Canvas } from "react-three-fiber";
 import * as THREE from "three";
 
 import Effects from "./Effects"
-
-const WIDTH = 400
-const HEIGHT = WIDTH
-
-const _object = new THREE.Object3D();
-const _color = new THREE.Color();
-
-function useImage(url) {
-  const [imageData, setImageData] = useState([]);
-  const [columns, setColumns] = useState(1);
-  const [rows, setRows] = useState(1);
-
-  useEffect(() => {
-    const img = new Image();
-    img.crossOrigin = "Anonymous";
-    img.src = url;
-
-    img.onload = function() {
-      const canvas = document.createElement("canvas");
-      const ctx = canvas.getContext("2d");
-
-      canvas.width = WIDTH;
-      canvas.height = HEIGHT;
-      ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
-
-      const imageData = ctx.getImageData(0, 0, canvas.width , canvas.height );
-
-      setImageData(imageData.data);
-      setColumns(parseInt(canvas.width, 10));
-      setRows(parseInt(canvas.height, 10));
-    };
-  }, [url, setImageData]);
-
-  const [colors, alpha] = useMemo(() => {
-    const color = new Float32Array(imageData.length * 4);
-    const alpha = [];
-
-    for (let i = 0, j = 0; i < imageData.length; i += 4, j += 1) {
-      _color.set(
-        `rgb( ${imageData[i]} , ${imageData[i + 1]} , ${imageData[i + 2]})`
-      );
-      _color.toArray(color, j * 3);
-      alpha.push(imageData[i + 3])
-    }
-    return [color, alpha];
-  }, [imageData]);
-
-  return { colors, alpha, columns, rows };
-}
-
-function Pixeloni(props) {
-  const { colors, alpha, columns, rows } = props
-
-  const ref = useRef();
-  const attrib = useRef();
-
-  useFrame(state => {
-    if (ref.current && attrib.current) {
-      const time = state.clock.getElapsedTime()
-
-      // ref.current.rotation.z = 2 * Math.PI * time / 100
-      // ref.current.rotation.x = 2 * Math.PI * time / 100
-      // ref.current.rotation.y = 2 * Math.PI * time / 100
-      
-      let i = 0;
-      for (let y = 0; y < rows; y++) {
-        for (let x = 0; x < columns; x++) {
-          const id = i++;
-
-          attrib.current.needsUpdate = true;
-          
-          _object.position.set(
-            -(WIDTH / 2) + x,
-            (HEIGHT / 2) - y,
-            -(WIDTH)// + WIDTH * 0.01 * Math.cos(time * y * WIDTH / 5000) 
-            );
-            
-          _object.updateMatrix();
-            
-          if (alpha[id] !== 0) {
-            ref.current.setMatrixAt(id, _object.matrix);
-          }
-        }
-      }
-      ref.current.instanceMatrix.needsUpdate = true;
-    }
-  });
-
-  return (
-    <instancedMesh ref={ref} args={[null, null, colors.length / 3]}>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]}>
-        <instancedBufferAttribute
-          ref={attrib}
-          attachObject={["attributes", "color"]}
-          args={[colors, 3]}
-        />
-      </boxBufferGeometry>
-      <meshPhongMaterial attach="material" vertexColors={THREE.VertexColors} />
-    </instancedMesh>
-  );
-}
+import Background from "./Background";
+import Pixeloni from "./Pixeloni";
+import useImage from "./useImage";
+import Jumbo from "./Jumbo";
+import FatLines from "./FatLines";
 
 export default function App() {
   const { colors, alpha, columns, rows } = useImage(
     "pop.png"
   );
+
   return (
     <Canvas
-      gl={{ antialias: true, alpha: true }}
-      camera={{ position: [0, 0, 0], near: 5, far: 9000 }}
+      gl={{ antialias: false, alpha: false }}
+      camera={{ position: [0, 0, 0], near: 5, far: 1000 }}
       onCreated={({ gl }) => {
         gl.toneMapping = THREE.Uncharted2ToneMapping;
-        gl.setClearColor(new THREE.Color("lightpink"));
+        gl.setClearColor(new THREE.Color("#ea7bbe"));
       }}
     >
-      <ambientLight />
-      {/* <pointLight position={[0, 0, 0]} intensity={0.55} /> */}
-      <Pixeloni colors={colors} alpha={alpha} columns={columns} rows={rows} />
-      {/* <Effects /> */}
+      <Suspense fallback={null}>
+        <ambientLight />
+        <pointLight position={[0, 0, 0]} intensity={0.2} />
+        <Jumbo />
+        {/* <Pixeloni colors={colors} alpha={alpha} columns={columns} rows={rows} /> */}
+        <Background />
+        <FatLines count={25} />
+        <Effects />
+      </Suspense>
     </Canvas>
   );
 }
--- src/Effects.js
+++ src/Effects.js
@@ -4,34 +4,57 @@ import { extend, useThree, useFrame } from 'react-three-fiber'
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass'
-import { FilmPass } from 'three/examples/jsm/postprocessing/FilmPass'
 import { GlitchPass } from './post/Glitchpass'
 import { WaterPass } from './post/Waterpass'
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass'
 import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader'
 import { SSAOPass } from 'three/examples/jsm/postprocessing/SSAOPass'
+import { OutlinePass } from 'three/examples/jsm/postprocessing/OutlinePass';
+import { FilmPass } from 'three/examples/jsm/postprocessing/FilmPass'
 
-extend({ EffectComposer, ShaderPass, RenderPass, WaterPass,SSAOPass, UnrealBloomPass, FilmPass, GlitchPass })
+import useStore from "./store"
 
-export default function Effects(x) {
+extend({ FilmPass, EffectComposer, ShaderPass, RenderPass, WaterPass,SSAOPass, UnrealBloomPass, FilmPass, GlitchPass, OutlinePass })
+
+export default function Effects() {
+  const objs = useStore(state => state.objs)
+
   const composer = useRef()
+  const outline = useRef()
+
   const { scene, gl, size, camera } = useThree()
   const aspect = useMemo(() => new THREE.Vector2(size.width, size.height), [size])
+
   useEffect(() => void composer.current.setSize(size.width, size.height), [size])
+
+  useEffect(() => { 
+    if (outline.current) {
+      outline.current.edgeStrength = 10
+      outline.current.edgeGlow = 0
+      outline.current.edgeThickness = 2
+      outline.current.visibleEdgeColor = new THREE.Color(0xf8f0f9)
+      outline.current.hiddenEdgeColor = new THREE.Color(0xf8f0f9)
+      outline.current.selectedObjects = objs
+    }
+  }, [objs, outline.current])
+
   useFrame(() => composer.current.render(), 1)
+
   return (
-    <effectComposer ref={composer} args={[gl]}>
-        <renderPass attachArray="passes" scene={scene} camera={camera} />
-        {/* <waterPass attachArray="passes" factor={1.5} /> */}
-        <unrealBloomPass attachArray="passes" args={[aspect, 2, 1, 1]} />
-        {/* <glitchPass attachArray="passes" factor={0.5} /> */}
-        {/* <shaderPass
+      <effectComposer ref={composer} args={[gl]}>
+          <renderPass attachArray="passes" scene={scene} camera={camera} />
+          <waterPass attachArray="passes" factor={0.2} />
+          {/* <unrealBloomPass attachArray="passes" args={[aspect, 1, 0.3, 0.9]} /> */}
+          {/* <glitchPass attachArray="passes" factor={0.5} /> */}
+          <outlinePass ref={outline} attachArray="passes" args={[aspect, scene, camera]} />
+          <filmPass attachArray="passes" args={[0.05, 0.4, 1500, false]} />
+          {/* <sSAOPass attachArray="passes" args={[scene, camera]} kernelRadius={0.6} maxDistance={0.03} />
+          <shaderPass
             attachArray="passes"
             args={[FXAAShader]}
             material-uniforms-resolution-value={[1 / size.width, 1 / size.height]}
             renderToScreen
-        /> */}
-        {/* <sSAOPass attachArray="passes" args={[scene, camera]} kernelRadius={0.6} maxDistance={0.03} /> */}
-    </effectComposer>
+          /> */}
+      </effectComposer>
   )
 }
